532a647260a39822082efc29a303a9d84ba0c7a4
Sofia Dinatolo <92044969+cockatiella@users.noreply.github.com>
2022-06-21 11:49:48 +0200

    fix

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const prompt = require('prompt-sync')({
+    signal: true
+});
+
+
 //make an array of movies
 let movies = ['matrix reloaded', 'god of gamblers', 'kung fu hustle', 'inglorious bastards', 'reservoir dogs', 'scarface', 'the departed', 'the exorcist', 'terminator', 'resident evil', 'clueless', 'legally blonde'];
 
@@ -8,25 +13,36 @@ let randomMovie = movies[Math.floor(Math.random() * movies.length)];
 
 let answerArray = [];
 for (let i = 0; i < randomMovie.length; i++) {
-    answerArray[i] = '_';
+    if (randomMovie[i] === ' ') {
+        answerArray[i] = ' ';
+    } else {
+        answerArray[i] = '_';
+    }
+
 }
 //create a variable to track the number of remaining letters
 let remainingLetters = randomMovie.length;
+let attempts = 0;
+let emojis = `¯\\_(:/)_/¯`;
+let displayEmoji = []
+
 
 //The main game loop:
 
 //while the player still has letters to guess
-while (remainingLetters > 0) {
+while (attempts < emojis.length) {
+    console.clear()
+    console.log(displayEmoji.join(''))
     //show the player their progress
-    alert(answerArray.join(' '));
+    console.log(answerArray.join(' '));
     //get a guess from the player
-    let guess = prompt('Guess a letter, or click Cancel to stop playing.');
+    let guess = prompt('Guess a letter. The result will be a movie name, or click Cancel to stop playing.');
     //if the guess is invalid, let the player know
     if (guess === null) {
         break;//exit the game loop
     } else if (guess.length !== 1) {
-        alert('Please enter a single letter.');
-    } else {
+        console.log('Please enter a single letter.');
+    } else if (randomMovie.includes(guess)) {
         //update the game state with the guess
         for (let j = 0; j < randomMovie.length; j++) {
             if (randomMovie[j] === guess) {
@@ -34,11 +50,18 @@ while (remainingLetters > 0) {
                 remainingLetters--; //subtract from the number of letters remaining
             }
         }
+    } else {
+        displayEmoji.push(emojis[attempts])
+        attempts++;
+        if (attempts === emojis.length) {
+            console.log('You lose')
+
+        }
     }
     // end of the game loop
 }
 //let player know the answer
-alert(answerArray.join(' '));
-alert('The answer was ' + randomMovie);
-alert('Thanks for playing!');
+console.log(answerArray.join(' '));
+console.log('The answer was ' + randomMovie);
+console.log('Thanks for playing!');
 
